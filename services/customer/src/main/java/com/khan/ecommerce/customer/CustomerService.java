2c6b30ff7086c38c51713c6859e97c07caa18554
subhan-99 <subhansoft9919@gmail.com>
2025-10-06 18:52:43 +0530

    customer service

---
+++ services/customer/src/main/java/com/khan/ecommerce/customer/CustomerService.java
@@ -0,0 +1,9 @@
+package com.khan.ecommerce.customer;
+
+import jakarta.validation.Valid;
+
+public class CustomerService {
+    public String createCustomer(@Valid CustomerRequest request) {
+        return null;
+    }
+}
