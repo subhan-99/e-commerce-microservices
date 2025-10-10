805d37ca77940e066fedf64740b138eb4679140a
subhan-99 <subhansoft9919@gmail.com>
2025-10-10 22:55:38 +0530

    today commit

--- services/customer/src/main/java/com/khan/ecommerce/handler/GlobalExceptionHandler.java
+++ services/customer/src/main/java/com/khan/ecommerce/handler/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.khan.ecommerce.handler;
 
+
 import com.khan.ecommerce.exception.CustomerNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,29 +10,30 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
-import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-
     @ExceptionHandler(CustomerNotFoundException.class)
-    public ResponseEntity<String> handle(CustomerNotFoundException exp) {
+    public ResponseEntity<String> handle(CustomerNotFoundException exp){
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
                 .body(exp.getMsg());
     }
-
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorResponse> handle(MethodArgumentNotValidException exp) {
-        Map<String, String> errors = new HashMap<>();
-        exp.getBindingResult().getAllErrors().forEach(error -> {
-            String fieldName = ((FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
-        });
+    public ResponseEntity<ErrorResponse> handle(MethodArgumentNotValidException exp){
+        var errors = new HashMap<String, String>();
+        exp.getBindingResult().getAllErrors()
+                .forEach(error ->{
+                    var fieldName = ((FieldError)error).getField();
+                    var errorMessage = error.getDefaultMessage();
+                    errors.put(fieldName, errorMessage);
+
+                });
+
 
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(new ErrorResponse(errors));
+
     }
 }
